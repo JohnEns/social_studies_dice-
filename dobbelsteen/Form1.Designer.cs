9ae699352f60aca222af58bc1f260964190ffd97
John Ensberg <John.Ensberg@gmail.com>
2016-05-30 22:02:36 +0200

    Educational product to support teachers in playing a game which teaches the pupils social studies.
    Alfa version (already tested in live situations)

---
+++ dobbelsteen/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Dobbelsteen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.werpButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.GamePanel = new System.Windows.Forms.Panel();
+            this.IntroPic = new System.Windows.Forms.PictureBox();
+            this.StopCountdown = new System.Windows.Forms.Button();
+            this.CountDownbutton = new System.Windows.Forms.Button();
+            this.CountDownLabel = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.getal1PictureBox = new System.Windows.Forms.PictureBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.MainMenuPanel = new System.Windows.Forms.Panel();
+            this.StartGameButton = new System.Windows.Forms.Button();
+            this.MainMenuPicture = new System.Windows.Forms.PictureBox();
+            this.MainMenuTitle = new System.Windows.Forms.Label();
+            this.CountDownTimer = new System.Windows.Forms.Timer(this.components);
+            this.GamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IntroPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getal1PictureBox)).BeginInit();
+            this.MainMenuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainMenuPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // werpButton
+            // 
+            this.werpButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.werpButton.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.werpButton.Location = new System.Drawing.Point(101, 393);
+            this.werpButton.Name = "werpButton";
+            this.werpButton.Size = new System.Drawing.Size(188, 38);
+            this.werpButton.TabIndex = 0;
+            this.werpButton.Text = "Werp dobbelsteen!";
+            this.werpButton.UseVisualStyleBackColor = false;
+            this.werpButton.Click += new System.EventHandler(this.WerpButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(153, -2);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(63, 24);
+            this.ScoreLabel.TabIndex = 6;
+            this.ScoreLabel.Text = "label1";
+            // 
+            // GamePanel
+            // 
+            this.GamePanel.Controls.Add(this.IntroPic);
+            this.GamePanel.Controls.Add(this.StopCountdown);
+            this.GamePanel.Controls.Add(this.CountDownbutton);
+            this.GamePanel.Controls.Add(this.CountDownLabel);
+            this.GamePanel.Controls.Add(this.ExitButton);
+            this.GamePanel.Controls.Add(this.werpButton);
+            this.GamePanel.Controls.Add(this.ScoreLabel);
+            this.GamePanel.Controls.Add(this.getal1PictureBox);
+            this.GamePanel.Controls.Add(this.ResultLabel);
+            this.GamePanel.Location = new System.Drawing.Point(107, 47);
+            this.GamePanel.Name = "GamePanel";
+            this.GamePanel.Size = new System.Drawing.Size(526, 451);
+            this.GamePanel.TabIndex = 8;
+            // 
+            // IntroPic
+            // 
+            this.IntroPic.Image = ((System.Drawing.Image)(resources.GetObject("IntroPic.Image")));
+            this.IntroPic.Location = new System.Drawing.Point(35, 74);
+            this.IntroPic.Name = "IntroPic";
+            this.IntroPic.Size = new System.Drawing.Size(410, 260);
+            this.IntroPic.TabIndex = 10;
+            this.IntroPic.TabStop = false;
+            // 
+            // StopCountdown
+            // 
+            this.StopCountdown.Location = new System.Drawing.Point(376, 242);
+            this.StopCountdown.Name = "StopCountdown";
+            this.StopCountdown.Size = new System.Drawing.Size(105, 23);
+            this.StopCountdown.TabIndex = 11;
+            this.StopCountdown.Text = "Stop de Klok!";
+            this.StopCountdown.UseVisualStyleBackColor = true;
+            this.StopCountdown.Click += new System.EventHandler(this.StopCountdown_Click);
+            // 
+            // CountDownbutton
+            // 
+            this.CountDownbutton.Location = new System.Drawing.Point(376, 242);
+            this.CountDownbutton.Name = "CountDownbutton";
+            this.CountDownbutton.Size = new System.Drawing.Size(105, 23);
+            this.CountDownbutton.TabIndex = 10;
+            this.CountDownbutton.Text = "Start de Klok";
+            this.CountDownbutton.UseVisualStyleBackColor = true;
+            this.CountDownbutton.Click += new System.EventHandler(this.CountDownbutton_Click);
+            // 
+            // CountDownLabel
+            // 
+            this.CountDownLabel.AutoSize = true;
+            this.CountDownLabel.Font = new System.Drawing.Font("Rockwell", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountDownLabel.Location = new System.Drawing.Point(378, 140);
+            this.CountDownLabel.Name = "CountDownLabel";
+            this.CountDownLabel.Size = new System.Drawing.Size(103, 77);
+            this.CountDownLabel.TabIndex = 9;
+            this.CountDownLabel.Text = "30";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ExitButton.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(448, 0);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 24);
+            this.ExitButton.TabIndex = 8;
+            this.ExitButton.Text = "Stop Spel";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // getal1PictureBox
+            // 
+            this.getal1PictureBox.Image = global::Dobbelsteen.Properties.Resources.dice6;
+            this.getal1PictureBox.Location = new System.Drawing.Point(79, 110);
+            this.getal1PictureBox.Name = "getal1PictureBox";
+            this.getal1PictureBox.Size = new System.Drawing.Size(234, 224);
+            this.getal1PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.getal1PictureBox.TabIndex = 1;
+            this.getal1PictureBox.TabStop = false;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLabel.Location = new System.Drawing.Point(73, 23);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(137, 33);
+            this.ResultLabel.TabIndex = 5;
+            this.ResultLabel.Text = "ResultLabel";
+            // 
+            // MainMenuPanel
+            // 
+            this.MainMenuPanel.Controls.Add(this.StartGameButton);
+            this.MainMenuPanel.Controls.Add(this.MainMenuPicture);
+            this.MainMenuPanel.Controls.Add(this.MainMenuTitle);
+            this.MainMenuPanel.Location = new System.Drawing.Point(651, 47);
+            this.MainMenuPanel.Name = "MainMenuPanel";
+            this.MainMenuPanel.Size = new System.Drawing.Size(526, 451);
+            this.MainMenuPanel.TabIndex = 9;
+            // 
+            // StartGameButton
+            // 
+            this.StartGameButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.StartGameButton.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartGameButton.Location = new System.Drawing.Point(185, 393);
+            this.StartGameButton.Name = "StartGameButton";
+            this.StartGameButton.Size = new System.Drawing.Size(135, 38);
+            this.StartGameButton.TabIndex = 9;
+            this.StartGameButton.Text = "Start Spel!";
+            this.StartGameButton.UseVisualStyleBackColor = false;
+            this.StartGameButton.Click += new System.EventHandler(this.StartGameButton_Click);
+            // 
+            // MainMenuPicture
+            // 
+            this.MainMenuPicture.Image = ((System.Drawing.Image)(resources.GetObject("MainMenuPicture.Image")));
+            this.MainMenuPicture.Location = new System.Drawing.Point(35, 74);
+            this.MainMenuPicture.Name = "MainMenuPicture";
+            this.MainMenuPicture.Size = new System.Drawing.Size(410, 260);
+            this.MainMenuPicture.TabIndex = 8;
+            this.MainMenuPicture.TabStop = false;
+            this.MainMenuPicture.Click += new System.EventHandler(this.StartGameButton_Click);
+            // 
+            // MainMenuTitle
+            // 
+            this.MainMenuTitle.AutoSize = true;
+            this.MainMenuTitle.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuTitle.Location = new System.Drawing.Point(60, 23);
+            this.MainMenuTitle.Name = "MainMenuTitle";
+            this.MainMenuTitle.Size = new System.Drawing.Size(368, 39);
+            this.MainMenuTitle.TabIndex = 0;
+            this.MainMenuTitle.Text = "Het spel van mevrouw Knel";
+            // 
+            // CountDownTimer
+            // 
+            this.CountDownTimer.Interval = 1000;
+            this.CountDownTimer.Tick += new System.EventHandler(this.CountDownTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 526);
+            this.Controls.Add(this.MainMenuPanel);
+            this.Controls.Add(this.GamePanel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dobbelsteen";
+            this.GamePanel.ResumeLayout(false);
+            this.GamePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IntroPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getal1PictureBox)).EndInit();
+            this.MainMenuPanel.ResumeLayout(false);
+            this.MainMenuPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainMenuPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button werpButton;
+        private System.Windows.Forms.PictureBox getal1PictureBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Panel GamePanel;
+        private System.Windows.Forms.Panel MainMenuPanel;
+        private System.Windows.Forms.Button StartGameButton;
+        private System.Windows.Forms.PictureBox MainMenuPicture;
+        private System.Windows.Forms.Label MainMenuTitle;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label CountDownLabel;
+        private System.Windows.Forms.Button CountDownbutton;
+        private System.Windows.Forms.Timer CountDownTimer;
+        private System.Windows.Forms.Button StopCountdown;
+        private System.Windows.Forms.PictureBox IntroPic;
+    }
+}
+
---
+++ dobbelsteen/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace Dobbelsteen
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Random object instantie.
+        /// </summary>
+        private Random _random = new Random();
+
+        /// <summary>
+        /// Houdt de tijd bij waarop op de knop werpButton is geklikt.
+        /// </summary>
+        private DateTime _startedAt;
+
+        /// <summary>
+        /// Willekeurige tijdsinterval waarop het gooien van de dobbelsteen moet stoppen.
+        /// </summary>
+        private int _willekeurigeTijd;
+
+        /// <summary>
+        /// Dit integer houdt de tijd bij van de 30 seconden countdown
+        /// </summary>
+        private int timeLeft;
+
+
+        /// private Dobbellogica = new Dobbellogica();
+
+        /// <summary>
+        /// Lijst van dobbelsteen plaatjes (zie in Solution explorer: Dobbelsteen > Properties > Resources.resx.
+        /// </summary>
+        private Image[] _images = new Image[6]
+        {
+            Properties.Resources.dice1,
+            Properties.Resources.dice2,
+            Properties.Resources.dice3,
+            Properties.Resources.dice4,
+            Properties.Resources.dice5,
+            Properties.Resources.dice6
+        };
+
+        /// <summary>
+        /// Lijst van dobbelsteen geluiden.
+        /// </summary>
+        private Stream[] _sounds = new Stream[6]
+        {
+            Properties.Resources.dob1,
+            Properties.Resources.dob2,
+            Properties.Resources.dob3,
+            Properties.Resources.dob4,
+            Properties.Resources.dob5,
+            Properties.Resources.dob6
+        };
+
+        /// <summary>
+        /// Lijst van dobbelsteen geluiden.
+        /// </summary>
+        private Stream[] _soundsSONG = new Stream[6]
+        {
+            Properties.Resources._1___Arctic_Monkeys___No__1_Party_Anthem__short,
+            Properties.Resources._2___Beyoncé__1_1,
+            Properties.Resources._3___K3___1_2_3,
+            Properties.Resources._4___don_omar_conteo,
+            Properties.Resources._5_O_Clock_in_the_morning,
+            Properties.Resources._6___Beyoncé___Drunk_in_Love
+        };
+
+        /// <summary>
+        /// Lijst van opdrachten.
+        /// </summary>
+        private string[] _tasks = new string[6]
+        {
+            "Open vraag - zelf beantwoorden",
+            "Begrippen - zelf omschrijven",
+            "Gesloten vraag voor alle teams",
+            "Kies een Begrip, een Open of een Gesloten vraag voor alle teams",
+            "Begrippen - team omschrijft",
+            "Kies een Begrip, een Open of een Gesloten vraag om zelf te doen"
+        };
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGameUi();
+        }
+
+        /// <summary>
+        /// Initialiseert de interface voor de game.
+        /// </summary>
+        private void InitializeGameUi()
+        {
+            var panelLocation = new Point(107, 47);
+            GamePanel.Visible = false;
+            GamePanel.Location = panelLocation;
+            MainMenuPanel.Location = panelLocation;
+            Size = new Size(716, 565);
+
+            ShowScoreLabels(false);
+            getal1PictureBox.Visible = false;
+            ResultLabel.MaximumSize = new Size(460, 71);
+            ResultLabel.AutoSize = true;
+            timeLeft = 30;
+            CountDownLabel.Visible = false;
+            CountDownbutton.Visible = false;
+            StopCountdown.Visible = false;
+        }
+
+        /// <summary>
+        /// Werp button click event handler. Gaat af als op de button werpButton geklikt wordt.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WerpButton_Click(object sender, EventArgs e)
+        {
+            _willekeurigeTijd = _random.Next(2000, 4000);
+            _startedAt = DateTime.Now;
+
+            Start();
+
+            werpButton.Text = "Bezig...";
+            werpButton.Enabled = false; // Schakel de button uit zolang de timer loopt
+
+            timer1.Start();
+        }
+
+        private void Start()
+        {
+            IntroPic.Visible = false;
+            IntroPic.SizeMode = PictureBoxSizeMode.StretchImage;
+            getal1PictureBox.Visible = true;
+            CountDownLabel.Visible = false;
+            CountDownbutton.Visible = false;
+
+            PlayAudioRollStart(); // Speel geluid beginnende rollende dobbelsteen af
+            PlayAudioRolling(); // Speel geluid rollende dobbelsteen af in een loop
+        }
+
+
+        /// <summary>
+        /// Speel de opgegeven audioclip af.
+        /// </summary>
+        /// <param name="audioClip"></param>
+        private void PlayAudio(Stream audioClip)
+        {
+            // Maak een kopie van de audio stream om het meerdere
+            // kereren tegelijk af te kunnen spelen
+            audioClip.Position = 0;
+            var audioClipCopy = new MemoryStream();
+            audioClip.CopyTo(audioClipCopy);
+            audioClipCopy.Position = 0;
+
+            using (SoundPlayer player = new SoundPlayer(audioClipCopy))
+            {
+                player.Play();
+            }
+        }
+
+        private void playaudio() // Speel geluid beginnende rollende dobbelsteen af
+        {
+            // Het geluid van een rollende dobbelsteen wordt afgespeeld
+            SoundPlayer audio3 = new SoundPlayer(Dobbelsteen.Properties.Resources.rolling);
+            audio3.Play();
+        }
+
+        private void PlayAudioRollStart() // Speel geluid beginnende rollende dobbelsteen af
+        {
+            // Het geluid van een rollende dobbelsteen wordt afgespeeld
+            SoundPlayer audio3 = new SoundPlayer(Dobbelsteen.Properties.Resources.rolling);
+            audio3.Play();
+        }
+
+        /// <summary>
+        /// Speel de audio loop rolling af.
+        /// </summary>
+        /// <param name="clip"></param>
+        private void PlayAudioRolling() // Speel geluid rollende dobbelsteen af in een loop
+        {
+            using (SoundPlayer player = new SoundPlayer(Properties.Resources.only_roll))
+            {
+                Properties.Resources.only_roll.Position = 0; // De leeskop voor wav-loop handmatig terugzetten op 0
+                player.Stream = null;    // De stream verwijderen uit het geheugen
+                player.Stream = Properties.Resources.only_roll;  // De wav verbinden aan audioroll
+                player.PlayLooping();          // Speel de clip af (deja vu?)
+            }
+        }
+
+        private void playaudiorollstop() // Speel geluid volledig rollende dobbelsteen af
+        {
+            // Het geluid van een rollende dobbelsteen wordt afgespeeld
+            SoundPlayer audio2 = new SoundPlayer(Properties.Resources.long_roll);
+            audio2.Play();
+        }
+
+        /// <summary>
+        /// Speel foutmelding geluidje af.
+        /// </summary>
+        private void PlayAudioError() // Speel geluid Fout (Error) af
+        {
+            // Het Fout-geluid wordt afgespeeld
+            PlayAudio(Properties.Resources.error);
+        }
+
+        /// <summary>
+        /// Werk de PictureBox van de dobbelsteen met een willekeurig nieuw plaatje.
+        /// </summary>
+        private void ThrowDice() // Genereert een willekeurig plaatje uit de images array
+        {
+            getal1PictureBox.Image = _images[_random.Next(0, 6)];
+
+        }
+
+        /// <summary>
+        /// Timer tick interval event handler. Gaat af elke keer als de interval van de timer voorbij is. 
+        /// Stopt de timer nadat willekeurigeTijd verstreken is.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            ThrowDice();
+
+            var delta = DateTime.Now - _startedAt;
+            if (delta.TotalMilliseconds >= _willekeurigeTijd)
+            {
+                RollEnded();
+            }
+        }
+
+        /// <summary>
+        /// Dit werkt de afhandeling af van de worp
+        /// </summary>
+        private void RollEnded()
+        {
+            // Stop de timer.
+            timer1.Stop();
+
+            // Stop het rollende geluid en laat de dobbelsteen uitrollen.
+            playaudiorollstop();
+
+            EvaluateResult();
+
+
+            // Herstel de button zodat de dobbelsteen opnieuw geworpen kan worden.
+            werpButton.Text = "Werp dobbelsteen!";
+            werpButton.Enabled = true;
+        }
+
+        /// <summary>
+        /// Evalueer het resultaat van de worp.
+        /// </summary>
+        private void EvaluateResult()
+        {
+            ShowScoreLabels(true);
+            var index = Array.IndexOf(_images, getal1PictureBox.Image);
+            var score = index + 1;
+
+            if (index == -1)
+            {
+                PlayAudioError();
+                MessageBox.Show("Sorry, er gebeurde iets onverwachts", "Je gooide veels te hard!");
+                return;
+            }
+
+            PlayAudio(_soundsSONG[index]);
+            ScoreLabel.Text = "Jouw worp was " + score;
+            ResultLabel.Text = _tasks[index];
+
+            CountDownReady();
+        }
+
+        /// <summary>
+        /// Toont of verbergt de score labels op basis van de opgeven staat.
+        /// </summary>
+        /// <param name="state"></param>
+        private void ShowScoreLabels(bool state)
+        {
+            ResultLabel.Visible = state;
+            ScoreLabel.Visible = state;
+        }
+
+        private void StartGameButton_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            MainMenuPanel.Visible = true;
+            GamePanel.Visible = false;
+        }
+
+        private void StartGame()
+        {
+            MainMenuPanel.Visible = false;
+            GamePanel.Visible = true;
+        }
+
+        private void EndGame()
+        {
+            MainMenuPanel.Visible = true;
+            GamePanel.Visible = false;
+        }
+
+        /// <summary>
+        /// Maakt de CountDown-teller + -knop zichtbaar
+        /// </summary>
+        private void CountDownReady()
+        {
+            CountDownLabel.Text = "30 ";
+            CountDownLabel.Visible = true;
+            CountDownbutton.Visible = true;
+            CountDownbutton.Text = "Start de Klok!";
+        }
+
+        /// <summary>
+        /// Na klikken op de Countdownbutton wordt de Werpbutton uitgeschakeld.
+        /// De StopCountDownknop wordt zichtbaar.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CountDownbutton_Click(object sender, EventArgs e)
+        {
+            CountDownReady();
+
+            werpButton.Enabled = false;
+            timeLeft = 29;
+            CountDownTimer.Start();
+
+            CountDownbutton.Visible = false;
+            StopCountdown.Visible = true;
+        }
+
+        /// <summary>
+        /// Elke seconde wordt het beeld ververst met de hoeveelheid tijd die er nog over is.
+        /// Het geluid van een tikkende klok loopt mee.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CountDownTimer_Tick(object sender, EventArgs e)
+        {
+            PlayAudio(Properties.Resources.clock);
+            CountDownLabel.Text = timeLeft + "";
+            timeLeft = timeLeft - 1;
+
+            if (timeLeft <10)
+            {
+                CountDownLabel.ForeColor = System.Drawing.Color.Red;
+            }
+
+            if (timeLeft == -1)
+            {
+                ResetCounter();
+                ResultLabel.Text = "De tijd is om!";
+                PlayAudio(Properties.Resources.BimBam);
+            }
+        }
+
+        /// <summary>
+        /// Stopt de CountDown. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StopCountdown_Click(object sender, EventArgs e)
+        {
+            ResetCounter();
+        }
+
+        private void ResetCounter()
+        {
+            CountDownTimer.Stop();
+            CountDownbutton.Visible = true;
+            CountDownbutton.Text = "Herstarten?";
+            StopCountdown.Visible = false;
+            werpButton.Enabled = true;
+        }
+
+
+    }
+}
